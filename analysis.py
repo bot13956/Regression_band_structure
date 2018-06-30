bc79fcb00ea9e012cf8af004f100b80db5328aaa
Benjamin Obi Tayo <benjaminobi@gmail.com>
2018-06-30 07:04:18 -0500

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,7 +3,7 @@
 Created on Fri Jun 29 20:52:35 2018
 
 Code performs linear regression on simulated band structure data for MoS2. For effective mass calculations,
-we use a quadratic (parabolic) fit (degree = 2) for our data. This is example where knowledge of the behavior 
+we use a quadratic (parabolic) fit (degree = 2) for our data. This is an example where knowledge of the behavior 
 of the dispersion relation in the vicinity of the conduction band minimum  guides us to choose a quadratic fit. 
 Even though a higher degree polynomial fit, e.g. degree = 4 produces a better accuracy (higher R-squared value), 
 it leads to overfitting of the model.
